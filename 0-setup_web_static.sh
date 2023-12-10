e1738329b793d3c03f1047fa816a19aaaf5b1b6f
Beekaz <sooperfect00@gmail.com>
2023-12-10 15:50:00 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,8 +9,8 @@ mkdir -p /data/web_static/shared/
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+chown -R beekaz:beekaz /data/
+chgrp -R beekaz /data/
 
 printf %s "server {
     listen 80 default_server;
